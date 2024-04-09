77c2d33bbd7210ad1e297f4cc3fc71cf7e30cbf0
ViniciusPolo <admviniciuspolo@gmail.com>
2024-04-09 14:04:03 -0300

    inutilizando rediz para hospedagem de forma gratuíta no clever-cloud

--- src/movie/movie.controller.ts
+++ src/movie/movie.controller.ts
@@ -12,15 +12,15 @@ import {
 import { FindAllParameters, MovieDto, MovieRouteParameters } from './movie.dto';
 import { MovieService } from './movie.service';
 import { AuthGuard } from '../auth/auth.guard';
-import { RedisService } from '../redis/redis.service'; 
-import { title } from 'process';
+// import { RedisService } from '../redis/redis.service'; 
+
 
 @UseGuards(AuthGuard)
 @Controller('movie')
 export class MovieController {
   constructor(
     private readonly movieService: MovieService,
-    private readonly redisService: RedisService, 
+    // private readonly redisService: RedisService, 
   ) { }
   
 
@@ -32,36 +32,36 @@ export class MovieController {
 
   @Get('/:id')
   async findById(@Param('id') id: string) {
-    const cachedMovie = await this.redisService.get(id); 
-    if (cachedMovie) {
-      return JSON.parse(cachedMovie);
-    }
+    // const cachedMovie = await this.redisService.get(id); 
+    // if (cachedMovie) {
+    //   return JSON.parse(cachedMovie);
+    // }
     return this.movieService.findById(id);
   }
 
   @Get()
   async findAll(@Query() params: FindAllParameters): Promise<MovieDto[]> {
-    if(!params){
-      const cachedMovies = await this.redisService.get('allMovies'); 
-      if (cachedMovies) {
-        return JSON.parse(cachedMovies); 
-      }
-    }
+    // if(!params){
+    //   const cachedMovies = await this.redisService.get('allMovies'); 
+    //   if (cachedMovies) {
+    //     return JSON.parse(cachedMovies); 
+    //   }
+    // }
     const movies = await this.movieService.findAll(params);
-    await this.redisService.set('allMovies', JSON.stringify(movies), 50000); 
+    // await this.redisService.set('allMovies', JSON.stringify(movies), 50000); 
     return movies;
   }
 
   @Put('/:id')
   async update(@Param() params: MovieRouteParameters, @Body() movie: MovieDto) {
     await this.movieService.update(params.id, movie);
-    await this.redisService.set(params.id, JSON.stringify(movie), 50000); 
+    // await this.redisService.set(params.id, JSON.stringify(movie), 50000); 
 
   }
 
   @Delete('/:id')
   async remove(@Param('id') id: string): Promise<void> {
     await this.movieService.remove(id);
-    this.redisService.delete;
+    // this.redisService.delete;
   }
 }
